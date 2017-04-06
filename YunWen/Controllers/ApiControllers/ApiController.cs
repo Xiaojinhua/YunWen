6a0b38e05c3736d0072d39e4ccdeb71602d99d24
xiaojinhua <xiaojinhua@sodao.com>
2017-04-06 10:21:06 +0800

    添加json解析库、相关思路流程

--- YunWen/Controllers/ApiControllers/ApiController.cs
+++ YunWen/Controllers/ApiControllers/ApiController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using YunWen.Models;
+using YunWen.Helper;
+using System.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace YunWen.Controllers
 {
@@ -17,6 +21,34 @@ namespace YunWen.Controllers
         /// <returns></returns>
         public ActionResult GetInfo(string longitude, string latitude)
         {
+            #region 通过经纬度获取地址信息
+            var url = ConfigurationManager.AppSettings["GeoApiUrl"];
+            var key = ConfigurationManager.AppSettings["BaiduMapKey"];
+            var dic = new Dictionary<string, string>()
+            {
+                {"ak", key },
+                {"location", latitude + "," + longitude },
+                {"output","json" }
+            };
+            var addressInfo = HttpHelper.Get(url, dic);
+            var address = "";
+            try
+            {
+                var addressDes = (JObject)JsonConvert.DeserializeObject(addressInfo);
+                address = addressDes["result"]["formatted_address"].ToString();
+            }
+            catch (Exception ex)
+            {
+            }
+            #endregion
+
+            #region 通过获取的地址，在数据库里面检索商家
+            #endregion
+
+            #region 再通过商家地址，反查GPS信息
+            #endregion
+
+            //将商家GPS，以及相关信息返回
             var suppliers = new List<SupplierGeoModel>();
             suppliers.Add(new SupplierGeoModel()
             {
--- YunWen/Helper/HttpHelper.cs
+++ YunWen/Helper/HttpHelper.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace Helper
+namespace YunWen.Helper
 {
     /// <summary>
     /// 
